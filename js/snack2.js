420f0d92ee90970537081a51a233ed31a42d56b4
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-23 19:04:19 +0200

    snack2 punto2

--- js/snack2.js
+++ js/snack2.js
@@ -12,8 +12,36 @@ const nomiMaiuscolo = [];
 
 for (let index = 0; index < nomiStudenti.length; index++) {
     const nome = nomiStudenti[index];
-    const nomeMaiuscolo = nome.toUpperCase();
-    nomiMaiuscolo.push(nomeMaiuscolo);
+    const nomeMaiusc = nome.toUpperCase();
+    nomiMaiuscolo.push(nomeMaiusc);
 };
 
-console.log(nomiStudenti);
+console.log(nomiMaiuscolo);
+
+
+
+const studenti = [
+    { id: 101, nomeStudenti: 'Giuseppina della Rovere', totaleVoti: 85 },
+    { id: 102, nomeStudenti: 'Paola Cortellessa', totaleVoti: 55 },
+    { id: 103, nomeStudenti: 'Andrea Mantegna', totaleVoti: 78 },
+    { id: 104, nomeStudenti: 'Gaia Borromini', totaleVoti: 90 },
+    { id: 105, nomeStudenti: 'Luigi Grimaldello', totaleVoti: 65 },
+    { id: 106, nomeStudenti: 'Piero della Francesca', totaleVoti: 45 },
+    { id: 107, nomeStudenti: 'Francesca da Polenta', totaleVoti: 72 }
+];
+
+const studentiVotoAlto = [];
+for (let index = 0; index < studenti.length; index++) {
+    const studente = studenti[index];
+    if (studente.totaleVoti > 70) {
+        studentiVotoAlto.push(studente);
+    }
+};
+
+for (let index = 0; index < studentiVotoAlto.length; index++) {
+    const studente = studentiVotoAlto[index];
+    let nomeStudente = studente.nomeStudenti;
+    let risultato = "Lo studente " + nomeStudente + " ha voti sopra la media.";
+    console.log(risultato);
+};
+
