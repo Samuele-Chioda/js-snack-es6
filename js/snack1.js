9c9b70f7ba353ff634ef95637e2bcc233b22b6b0
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-23 16:21:01 +0200

    array invitati

---
+++ js/snack1.js
@@ -0,0 +1,16 @@
+
+const guestName = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'George Clooney',
+    'Fedez',
+    'Amadeus',
+    'Fiorello'
+];
+
+const tableName = 'Tavolo Vip';
+const place = [];
