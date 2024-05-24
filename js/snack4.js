7919f7e53f4cf6bfaa509d3823f283a42a242a82
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-24 19:05:19 +0200

    snack4

---
+++ js/snack4.js
@@ -0,0 +1,21 @@
+const squadre = [
+    { nome: 'Inter', punti: 0, falli: 0 },
+    { nome: 'Juve', punti: 0, falli: 0 },
+    { nome: 'Milan', punti: 0, falli: 0 },
+    { nome: 'Cagliari', punti: 0, falli: 0 },
+    { nome: 'Lecce', punti: 0, falli: 0 }
+];
+
+for (let index = 0; index < squadre.length; index++) {
+    const squadra = squadre[index];
+    squadra.punti = Math.floor(Math.random()*100);
+    squadra.falli = Math.floor(Math.random()*100);
+}
+
+const nomeFalli = [];
+
+for (let index = 0; index < squadre.length; index++) {
+    let { nome, falli } = squadre[index];
+    nomeFalli.push({nome, falli});
+}
+console.log(nomeFalli);
